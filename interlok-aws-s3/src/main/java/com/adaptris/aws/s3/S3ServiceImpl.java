bf038cb6d55b9853c3b9ce9ea1ffe84b89aea0b6
Lewin Chan <lewin.chan@adaptris.com>
2019-04-30 13:15:17 +0100

    INTERLOK-2771 Remove deprecated configuration.

--- interlok-aws-s3/src/main/java/com/adaptris/aws/s3/S3ServiceImpl.java
+++ interlok-aws-s3/src/main/java/com/adaptris/aws/s3/S3ServiceImpl.java
@@ -17,18 +17,13 @@
 package com.adaptris.aws.s3;
 
 import javax.validation.Valid;
-import com.adaptris.annotation.AdvancedConfig;
-import com.adaptris.annotation.Removal;
-import com.adaptris.aws.AWSAuthentication;
+
 import com.adaptris.core.AdaptrisConnection;
 import com.adaptris.core.ConnectedService;
 import com.adaptris.core.CoreException;
 import com.adaptris.core.ServiceImp;
 import com.adaptris.core.util.LifecycleHelper;
-import com.adaptris.core.util.LoggingHelper;
-import com.adaptris.util.KeyValuePairSet;
 import com.amazonaws.ClientConfiguration;
-import com.amazonaws.services.s3.AmazonS3Client;
 
 /**
  * Abstract implemention of {@link S3Service}
@@ -57,16 +52,6 @@ import com.amazonaws.services.s3.AmazonS3Client;
 public abstract class S3ServiceImpl extends ServiceImp implements ConnectedService {
 
   @Valid
-  @Deprecated
-  @AdvancedConfig
-  private AWSAuthentication authentication;
-
-  @Valid
-  @Deprecated
-  @AdvancedConfig
-  private KeyValuePairSet clientConfiguration;
-
-  @Valid
   private AdaptrisConnection connection;
   private transient boolean warningLogged;
   
@@ -75,22 +60,7 @@ public abstract class S3ServiceImpl extends ServiceImp implements ConnectedServi
 
   @Override
   public void prepare() throws CoreException {
-    handleDeprecatedConfig();
     LifecycleHelper.prepare(getConnection());
-  }
-
-  protected void handleDeprecatedConfig() {
-    if (getAuthentication() != null) {
-      log.warn("authentication is deprecated); use amazon-s3-connection instead");
-    }
-    if (getClientConfiguration() != null) {
-      log.warn("client-configuration is deprecated); use amazon-s3-connection instead");
-    }
-    if (getConnection() == null) {
-      LoggingHelper.logWarning(warningLogged, ()-> {warningLogged = true;}, 
-          "No Connection; using legacy authentication and client-configuration");      
-      setConnection(new AmazonS3Connection(getAuthentication(), getClientConfiguration()));
-    }
   }
   
   @Override
@@ -115,55 +85,7 @@ public abstract class S3ServiceImpl extends ServiceImp implements ConnectedServi
     LifecycleHelper.close(getConnection());
   }
 
-  @Deprecated
-  @Removal(version="3.9.0")
-  protected AmazonS3Client amazonClient() {
-    return getConnection().retrieveConnection(AmazonS3Connection.class).amazonClient();
-  }
-
-  /**
-   * @deprecated since 3.6.4 use a {@link AmazonS3Connection} instead.
-   */
-  @Deprecated
-  @Removal(version = "3.9.0", message = "use AmazonS3Connection as the connection instead")
-  public AWSAuthentication getAuthentication() {
-    return authentication;
-  }
-
-  /**
-   * The authentication method to use
-   * 
-   * @param a the authentication to use.
-   * @deprecated since 3.6.4 use a {@link AmazonS3Connection} instead.
-   */
-  @Deprecated
-  @Removal(version = "3.9.0", message = "use AmazonS3Connection as the connection instead")
-  public void setAuthentication(AWSAuthentication a) {
-    this.authentication = a;
-  }
-
-
-  /**
-   * @return the configurationBuilder
-   * @deprecated since 3.6.4 use a {@link AmazonS3Connection} instead.
-   */
-  @Deprecated
-  @Removal(version = "3.9.0", message = "use AmazonS3Connection as the connection instead")
-  public KeyValuePairSet getClientConfiguration() {
-    return clientConfiguration;
-  }
-
-
-  /**
-   * @param b the configurationBuilder to set
-   * @deprecated since 3.6.4 use a {@link AmazonS3Connection} instead.
-   */
-  @Deprecated
-  @Removal(version = "3.9.0", message = "use AmazonS3Connection as the connection instead")
-  public void setClientConfiguration(KeyValuePairSet b) {
-    this.clientConfiguration = b;
-  }
-
+  @Override
   public AdaptrisConnection getConnection() {
     return connection;
   }
@@ -173,6 +95,7 @@ public abstract class S3ServiceImpl extends ServiceImp implements ConnectedServi
    * 
    * @param connection the connection.
    */
+  @Override
   public void setConnection(AdaptrisConnection connection) {
     this.connection = connection;
   }
